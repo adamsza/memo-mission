3f1d6bdaebbed42c42feec020c5b8f61878fd2c8
Ádám Szalóky <szaloada@gmail.com>
2025-04-09 00:46:11 +0200

    Add card animations

--- src/components/CardBack.tsx
+++ src/components/CardBack.tsx
@@ -3,28 +3,34 @@ import cardBackImage from "../assets/card-back-image.svg";
 import cardBackHover from "../assets/card-back-hover.svg";
 import cardBackImageHover from "../assets/card-back-image-hover.svg";
 import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 
 
 function CardBack() {
     const [hover, setHover] = useState(false);
 
     return (
-        <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-            {hover ?
-                <div className="w-[100px] h-[150px] rounded-[10px] bg-white flex items-center justify-center p-1.25 shadow-[0_2px_5px_rgba(0,0,0,0.25)]">
-                    <div className="relative">
-                        <img src={cardBackHover}/>
-                        <img className="absolute left-1/2 top-1/2 -translate-1/2 " src={cardBackImageHover} />
-                    </div>
-                </div>
-                :
-                <div className="w-[100px] h-[150px] rounded-[10px] bg-white flex items-center justify-center p-1.25 shadow-[0_2px_1px_rgba(0,0,0,0.15)]">
-                    <div className="relative">
-                        <img src={cardBack}/>
-                        <img className="absolute left-1/2 top-1/2 -translate-1/2 " src={cardBackImage} />
-                    </div>
-                </div>
+        <div className="relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <AnimatePresence>
+            {hover &&
+                    <motion.div
+                        className="absolute z-1"
+                        key="hoveredCardBack" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                        <div className="w-[100px] h-[150px] rounded-[10px] bg-white flex items-center justify-center p-1.25 shadow-[0_2px_5px_rgba(0,0,0,0.25)]">
+                            <div className="relative">
+                                <img src={cardBackHover} />
+                                <img className="absolute left-1/2 top-1/2 -translate-1/2 " src={cardBackImageHover} />
+                            </div>
+                        </div>
+                    </motion.div>
             }
+            </AnimatePresence>
+            <div className="w-[100px] h-[150px] rounded-[10px] bg-white flex items-center justify-center p-1.25 shadow-[0_2px_1px_rgba(0,0,0,0.15)]">
+                <div className="relative">
+                    <img src={cardBack} />
+                    <img className="absolute left-1/2 top-1/2 -translate-1/2 " src={cardBackImage} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/CardBody.tsx
+++ src/components/CardBody.tsx
@@ -1,17 +1,39 @@
+import { AnimatePresence, motion } from "motion/react";
 import Card from "./Card";
 import CardBack from "./CardBack";
 import CardFront from "./CardFront";
 
 interface CardProps {
     card: Card,
-    onClick: (id: number) =>  void
+    onClick: (id: number) => void
 }
 
 function CardBody(props: CardProps) {
 
     return (
         <div className="flex justify-center" onClick={() => props.onClick(props.card.id)}>
-            {props.card.flipped ? <CardFront>{props.card.emoji}</CardFront> : <CardBack/>}
+            <AnimatePresence>
+                {props.card.flipped ?
+                    <motion.div
+                        className="absolute z-1"
+                        key="cardFront"
+                        initial={{ opacity: 0, scaleX: 0.5, translateX: "-25px" }}
+                        animate={{ opacity: 1, scaleX: 1, translateX: "0px" }}
+                        exit={{ opacity: 0, scaleX: 0.5, translateX: "-25px" }}
+                        transition={{duration: 0.2}}>
+                        <CardFront>{props.card.emoji}</CardFront>
+                    </motion.div>
+                    :
+                    <motion.div
+                        key="cardBack"
+                        initial={{ opacity: 0, scaleX: 0.5, translateX: "25px" }}
+                        animate={{ opacity: 1, scaleX: 1, translateX: "0px" }}
+                        exit={{ opacity: 0, scaleX: 0.5, translateX: "25px" }}
+                        transition={{duration: 0.2}}>
+                        <CardBack />
+                    </motion.div>
+                }
+            </AnimatePresence>
         </div>
     )
 }
