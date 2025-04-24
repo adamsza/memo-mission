d8b7b51d1a775bc248bb28febef84be93899be69
Ádám Szalóky <szaloada@gmail.com>
2025-04-24 22:59:35 +0200

    Fix cursor for settings

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -22,31 +22,31 @@ function SettingsModal(props: SettingsModalProps) {
         <div className="fixed top-0 left-0 w-lvw h-lvh bg-backdrop z-10">
             <div className="flex flex-col w-[300px] h-[350px] m-auto mt-15 rounded-[35px] bg-white overflow-hidden">
                 <div className="flex justify-between items-center bg-[#F5F5F5] py-6.25 px-5">
-                    <div className="font-gilroy-black text-2xl leading-4.25 text-black">Game settings</div>
-                    <FontAwesomeIcon className="text-[#D5D5D5] text-2xl px-1.25" icon={faTimes} onClick={props.handleClose} />
+                    <div className="font-gilroy-black text-2xl leading-4.25 text-black cursor-default">Game settings</div>
+                    <FontAwesomeIcon className="text-[#D5D5D5] text-2xl px-1.25 cursor-pointer" icon={faTimes} onClick={props.handleClose} />
                 </div>
                 <div className="h-full flex flex-col justify-between p-5">
                     <div className="flex justify-between items-center">
-                        <div className="font-gilroy-bold text-lg text-black">Name</div>
+                        <div className="font-gilroy-bold text-lg text-black cursor-default">Name</div>
                         <input className="h-10.25 box-border py-2.5 border-1 border-[#D5D5D5] rounded-[10px] font-gilroy text-lg text-center text-black"
                             value={name} onChange={(e) => dispatch(changeName(e.currentTarget.value))} />
                     </div>
                     <div className="flex justify-between items-center">
-                        <div className="font-gilroy-bold text-lg text-black">Number of pair of cards</div>
+                        <div className="font-gilroy-bold text-lg text-black cursor-default">Number of pair of cards</div>
                         <input className="w-12.5 h-10.25 box-border py-2.5 border-1 border-[#D5D5D5] rounded-[10px] font-gilroy text-lg text-center text-black"
                             value={cards} onChange={(e) => setCards(parseInt(e.currentTarget.value) || 0)} />
                     </div>
                     <div className="flex justify-between items-center">
-                        <div className="font-gilroy-bold text-lg text-black">Countdown time (sec.)</div>
+                        <div className="font-gilroy-bold text-lg text-black cursor-default">Countdown time (sec.)</div>
                         <input className="w-12.5 h-10.25 box-border py-2.5 border-1 border-[#D5D5D5] rounded-[10px] font-gilroy text-lg text-center text-black"
                             value={time} onChange={(e) => setTime(parseInt(e.currentTarget.value) || 0)} />
                     </div>
                     <div className="flex justify-between items-center">
-                        <div className="font-gilroy-bold text-lg text-black">Allowed bad guesses</div>
+                        <div className="font-gilroy-bold text-lg text-black cursor-default">Allowed bad guesses</div>
                         <input className="w-12.5 h-10.25 box-border py-2.5 border-1 border-[#D5D5D5] rounded-[10px] font-gilroy text-lg text-center text-black"
                             value={maxMistakes} onChange={(e) => setMaxMistakes(parseInt(e.currentTarget.value) || 0)} />
                     </div>
-                    <button className="w-full bg-[#FF3F56] rounded-[30px] py-3.75 px-5 text-white uppercase font-gilroy-bold text-[16px]"
+                    <button className="w-full bg-[#FF3F56] rounded-[30px] py-3.75 px-5 text-white uppercase font-gilroy-bold text-[16px] cursor-pointer"
                         onClick={() => {
                             dispatch(changeSettings({ cards: cards, time: time, maxMistakes: maxMistakes }));
                             props.handleClose();
